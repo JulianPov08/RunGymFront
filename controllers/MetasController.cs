a09ec39b2d69fe024706fb9c073d0cb6f6c8ad93
JulianPov08 <juliansena0809@gmail.com>
2025-02-27 21:03:07 -0500

    se crea el controller de metas y la vista metas

---
+++ controllers/MetasController.cs
@@ -0,0 +1,19 @@
+﻿using RunGymFront.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace RunGymFront.controllers
+{
+    public class MetasController : Controller 
+    {
+        public ActionResult Metas()
+        {
+            return View();
+        }
+    }
+}
